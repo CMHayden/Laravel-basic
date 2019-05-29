bd71154a4a088cd11d6103480f33c5203a371db4
CMHayden <haydencallum4@gmail.com>
2019-05-30 00:40:22 +0200

    Fixing issue on merging branches

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,17 +8,25 @@
     <title>Acme</title>
 </head>
 <body>
-    @include('inc.navbar')
+    <div class="wrapper">
+        @include('inc.navbar')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-lg-8">
-                @yield('content')
-            </div>
-            <div class="col-md-4 col-lg-4">
-                @include('inc.sidebar')
+        <div class="container">
+            @if(Request::is('/'))
+            @include('inc.showcase')
+            @endif
+            <div class="row">
+                <div class="col-md-8 col-lg-8">
+                    @yield('content')
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    @include('inc.sidebar')
+                </div>
             </div>
         </div>
     </div>
+    <footer id="footer" class="text-center">
+        <p>Copyright 2019 &copy; Acme </p>
+    </footer>
 </body>
 </html>
