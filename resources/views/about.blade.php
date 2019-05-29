437224e6aa354a928e3e2ed726da5f8db0693127
CMHayden <haydencallum4@gmail.com>
2019-05-29 20:58:26 +0200

    Fixes bug on home link

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,5 +3,13 @@
 @section('content')
 
 <h1>About</h1>
-
+<p>
+    Amet quidem facilis blanditiis, officia nihil necessitatibus perferendis tempore nobis reiciendis ut dolores expedita nostrum, optio sequi minus dignissimos nesciunt ea minima culpa in quasi laboriosam. Vero, unde. Fugiat, ad!
+    Eos facilis nostrum quidem quasi numquam ipsa commodi aliquam incidunt alias. Tempora necessitatibus inventore, culpa hic soluta a? Assumenda ducimus laudantium voluptatibus asperiores tempora officiis architecto autem doloremque sapiente minima.
+    Repudiandae placeat quibusdam iure quo id, repellendus velit minus vero voluptatum, accusantium dolor ex fugit deserunt, exercitationem obcaecati labore voluptates porro. Velit animi tempora delectus cum et neque similique itaque.
+    Harum blanditiis non dicta quidem exercitationem neque ipsum voluptates saepe illo mollitia animi deleniti, sint eaque atque quod debitis consectetur dolores tenetur quas! Quia totam provident deserunt inventore ex reprehenderit!
+    Ipsa, voluptatibus. Harum officiis placeat laborum magni amet consectetur eaque at sapiente! Rerum obcaecati aperiam sapiente dolores a commodi enim unde odit. Obcaecati officia, totam maxime fugit voluptates distinctio dolores!
+    Praesentium fuga officiis quaerat quas id expedita fugit, asperiores necessitatibus voluptatibus voluptatem consequuntur, aliquam, quidem illum ratione adipisci. Quae itaque ducimus eius assumenda aut reiciendis vero dolorum officiis, fuga dignissimos?
+    Aliquam quia consequatur accusantium rem temporibus quod perspiciatis, eos expedita cumque atque eius quo omnis, modi, non est at incidunt! Aliquid sapiente quos soluta sunt voluptatibus? Corrupti veniam ea animi!
+</p>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 <h1>Home</h1>
+<p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et in numquam modi quis blanditiis eos quas alias corrupti! Fuga quo necessitatibus iure neque, vel delectus laboriosam temporibus! Nesciunt, accusamus aliquid.
+    Dolorum reiciendis eum doloribus placeat fugit! Amet pariatur, laboriosam accusamus sunt enim totam quas placeat, fugit consequuntur ducimus in consectetur! Corporis perspiciatis architecto blanditiis vitae dolores eaque nulla eum saepe.
+    Nobis expedita quidem, mollitia veniam voluptas dolorum nulla animi odio rerum accusamus excepturi eveniet cum. Ipsa praesentium sint consectetur. Ea tenetur nam accusantium quisquam culpa natus officiis dignissimos fugit beatae!
+    Voluptatibus excepturi, quo corrupti cumque ipsa soluta sed earum ducimus placeat, expedita hic, sit nobis iste tempore provident eum inventore. Excepturi ullam hic dolorum laudantium animi delectus neque, error iure.
+    Vitae qui ratione libero neque! Quae maxime, officiis voluptatibus, impedit, minima autem nobis fuga accusamus nam fugit necessitatibus iste laudantium asperiores tenetur pariatur natus sit ducimus consequatur molestiae voluptate aperiam?
+</p>
 @endsection
 
 @section('sidebar')
