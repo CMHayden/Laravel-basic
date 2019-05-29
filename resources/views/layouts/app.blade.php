20fb06859e062afe83009d991c8c99b2b607a8c4
CMHayden <haydencallum4@gmail.com>
2019-05-29 21:09:07 +0200

    Adding some custom style and a footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
     @include('inc.navbar')
 
     <div class="container">
+        @if(Request::is('/'))
+        @include('inc.showcase')
+        @endif
         <div class="row">
             <div class="col-md-8 col-lg-8">
                 @yield('content')
@@ -20,5 +23,11 @@
             </div>
         </div>
     </div>
+
+    <footer id="footer" class="text-center">
+        <p>
+            Copyright 2019 &copy; ACME
+        </p>
+    </footer>
 </body>
 </html>
