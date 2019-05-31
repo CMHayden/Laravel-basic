36a7d82efd2f72ce0d51b7c03a2e0da1e8c9fc72
CMHayden <haydencallum4@gmail.com>
2019-05-31 15:58:15 +0200

    Adds Message Controller and validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
             <div class="row">
                 <div class="col-md-8 col-lg-8">
                     @yield('content')
+                    @include('inc.messages')
                 </div>
                 <div class="col-md-4 col-lg-4">
                     @include('inc.sidebar')
